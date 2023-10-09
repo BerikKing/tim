b03dd0eb12cb7d04a9fafb5158b170f325376b72
Berik Taibagar <beka@Beriks-MBP.lan>
2023-10-09 11:40:47 +0600

    First commit

---
+++ src/pages/auth/register.js
@@ -0,0 +1,164 @@
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Box, Button, Link, Stack, TextField, Typography } from '@mui/material';
+import { useAuth } from 'src/hooks/use-auth';
+import { Layout as AuthLayout } from 'src/layouts/auth/layout';
+
+const Page = () => {
+  const router = useRouter();
+  const auth = useAuth();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+      submit: null
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email('Жарамды email болуы керек')
+        .max(255)
+        .required('Email қажет'),
+      name: Yup
+        .string()
+        .max(255)
+        .required('Атыңызды жазыңыз'),
+      password: Yup
+        .string()
+        .max(255)
+        .required('Пароль қажет')
+    }),
+    onSubmit: async (values, helpers) => {
+      try {
+        await auth.signUp(values.email, values.name, values.password);
+        router.push('/');
+      } catch (err) {
+        helpers.setStatus({ success: false });
+        helpers.setErrors({ submit: err.message });
+        helpers.setSubmitting(false);
+      }
+    }
+  });
+
+  return (
+    <>
+      <Head>
+        <title>
+          Register
+        </title>
+      </Head>
+      <Box
+        sx={{
+          flex: '1 1 auto',
+          alignItems: 'center',
+          display: 'flex',
+          justifyContent: 'center'
+        }}
+      >
+        <Box
+          sx={{
+            maxWidth: 550,
+            px: 3,
+            py: '100px',
+            width: '100%'
+          }}
+        >
+          <div>
+            <Stack
+              spacing={1}
+              sx={{ mb: 3 }}
+            >
+              <Typography variant="h4">
+                Тіркелу
+              </Typography>
+              <Typography
+                color="text.secondary"
+                variant="body2"
+              >
+                Аккаунтыңыз бар ма?
+                &nbsp;
+                <Link
+                  component={NextLink}
+                  href="/auth/login"
+                  underline="hover"
+                  variant="subtitle2"
+                >
+                  Кіріңіз
+                </Link>
+              </Typography>
+            </Stack>
+            <form
+              noValidate
+              onSubmit={formik.handleSubmit}
+            >
+              <Stack spacing={3}>
+                <TextField
+                  error={!!(formik.touched.name && formik.errors.name)}
+                  fullWidth
+                  helperText={formik.touched.name && formik.errors.name}
+                  label="Атыңыз"
+                  name="name"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={formik.values.name}
+                />
+                <TextField
+                  error={!!(formik.touched.email && formik.errors.email)}
+                  fullWidth
+                  helperText={formik.touched.email && formik.errors.email}
+                  label="Email-ыңыз"
+                  name="email"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="email"
+                  value={formik.values.email}
+                />
+                <TextField
+                  error={!!(formik.touched.password && formik.errors.password)}
+                  fullWidth
+                  helperText={formik.touched.password && formik.errors.password}
+                  label="Пароль"
+                  name="password"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  type="password"
+                  value={formik.values.password}
+                />
+              </Stack>
+              {formik.errors.submit && (
+                <Typography
+                  color="error"
+                  sx={{ mt: 3 }}
+                  variant="body2"
+                >
+                  {formik.errors.submit}
+                </Typography>
+              )}
+              <Button
+                fullWidth
+                size="large"
+                sx={{ mt: 3 }}
+                type="submit"
+                variant="contained"
+              >
+                Жалғастыру
+              </Button>
+            </form>
+          </div>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => (
+  <AuthLayout>
+    {page}
+  </AuthLayout>
+);
+
+export default Page;
